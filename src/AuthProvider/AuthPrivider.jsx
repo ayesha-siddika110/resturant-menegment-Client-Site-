85624d9d90144553210b244b30ccc16250ccbf1a
Ayesha Siddika <ayeshasiddika777697@gmail.com>
2024-12-22 16:19:30 +0600

    project basic setup

---
+++ src/AuthProvider/AuthPrivider.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext()
+
+const AuthProvider = ({children}) => {
+    const [user,serUser] = useState([])
+
+
+    const authinfo = {
+        user
+    }
+    return (
+        <AuthContext.Provider value={authinfo}>
+            {children}
+            
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
---
+++ src/Router/PrivetRouter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PrivetRouter = ({children}) => {
+     
+    
+    
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PrivetRouter;
